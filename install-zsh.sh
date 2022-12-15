ed1ed75230ffdc5bbb14e0d116cb69e5b8e1edde
Xcalizorz <rzadjad+github@gmail.com>
2022-12-15 23:20:45 +0100

    Init: adjust i3 config to personal needs

---
+++ install-zsh.sh
@@ -0,0 +1,19 @@
+#/bin/bash
+set -euxo pipefail
+
+sudo pacman -Sy
+sudo pacman -S zsh
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+chsh -s $(which zsh)
+sudo chsh -s $(which zsh)
+
+cp .zshrc ~/.zshrc
+
+# Install plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
