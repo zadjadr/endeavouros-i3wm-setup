f55fddf7b843bfccb70bb7f94fb48eb9ebff1a05
Zadjad Rezai <rzadjad+github@gmail.com>
2022-12-21 09:42:15 +0100

    Add docker install script

--- custom-scripts/git-config.sh
+++ custom-scripts/git-config.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 set -euxo pipefail
 
 git config --global user.name "Zadjad Rezai"
---
+++ custom-scripts/install-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -exuo pipefail
+
+sudo pacman -Sy
+sudo pacman -S docker docker-buildx docker-compose
+
+sudo usermod -aG docker $(whoami)
