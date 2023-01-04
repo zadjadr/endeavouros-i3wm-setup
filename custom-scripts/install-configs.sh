b19a757027dabf9908f323397e9dc14974316310
Zadjad Rezai <rzadjad+github@gmail.com>
2023-01-04 17:21:03 +0100

    Add sway related changes

--- custom-scripts/install-configs.sh
+++ custom-scripts/install-configs.sh
@@ -9,39 +9,43 @@ set -euxo pipefail
 rm -rf \
 	~/.config/i3 \
 	~/.config/i3status-rust \
+	~/.config/sway \
+	~/.config/swaylock \
+	~/.config/waybar \
+	~/.config/mako \
+	~/.config/kanshi \
 	~/.config/alacritty \
 	~/.config/albert \
 	~/.config/autostart \
-	~/.config/autorandr \
-	~/.config/dunst \
 	~/.config/gtk-3.0 \
 	~/.config/gtk-4.0 \
 	~/.config/nitrogen \
 	~/.config/onedrive \
 	~/.config/ranger \
 	~/.config/rofi \
+	~/.config/wofi \
 	~/.config/yay
 
 ln -s \
 	"$PWD"/.config/i3 \
 	"$PWD"/.config/i3status-rust \
+	"$PWD"/.config/sway \
+	"$PWD"/.config/swaylock \
+	"$PWD"/.config/waybar \
+	"$PWD"/.config/mako \
+	"$PWD"/.config/kanshi \
 	"$PWD"/.config/alacritty \
 	"$PWD"/.config/albert \
 	"$PWD"/.config/autostart \
-	"$PWD"/.config/autorandr \
-    "$PWD"/.config/dunst \
 	"$PWD"/.config/gtk-3.0 \
 	"$PWD"/.config/gtk-4.0 \
 	"$PWD"/.config/nitrogen \
 	"$PWD"/.config/onedrive \
 	"$PWD"/.config/ranger \
 	"$PWD"/.config/rofi \
+	"$PWD"/.config/wofi \
 	"$PWD"/.config/yay \
 	~/.config
-
-## Xresources
-rm -rf ~/.Xresources
-ln -s "$PWD"/.Xresources ~/
 
 ## VScode
 rm -rf "$HOME/.config/Code - OSS/User/settings.json"
@@ -54,3 +58,5 @@ touch ~/.meta/status/pomodoro
 
 ## atuin cannot be symlinked
 cp -r "$PWD"/.config/atuin/config.toml "$HOME"/.config/atuin/config.toml
+rm -rf ~/.config/Code\ -\ OSS/User/settings.json
+ln -s "$PWD"/.config/Code\ -\ OSS/User/settings.json ~/.config/Code\ -\ OSS/User
