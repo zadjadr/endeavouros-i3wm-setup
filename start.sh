ddd00a68d18296fadf872d27beb6a04bed416b7d
Zadjad Rezai <rzadjad+github@gmail.com>
2023-01-05 17:14:07 +0100

    Separate albert install

--- start.sh
+++ start.sh
@@ -7,6 +7,11 @@
 ./custom-scripts/install-configs.sh
 ./custom-scripts/install-fonts.sh
 ./custom-scripts/install-acpid-events.sh
+# ./custom-scripts/install-albert.sh
+# ./custom-scripts/install-work-tools.sh
+# ./custom-scripts/install-acpid-events.sh
 ./custom-scripts/git-config.sh
+./custom-scripts/fix-login-issues.sh
+
 
 ./custom-scripts/install-zsh.sh 
