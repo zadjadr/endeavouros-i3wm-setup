040e51192cd93ec531f66ea8042491fdc6d9a9a8
Zadjad Rezai <rzadjad+github@gmail.com>
2023-01-06 22:03:28 +0100

    Cleanup fonts and icons

---
+++ custom-scripts/fix-icons.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+#
+set -euxo pipefail
+
+## fix hardcoded paths to icons
+SCRIPT_DIR=/tmp/hardcode-fixer
+git clone https://github.com/Foggalong/hardcode-fixer.git "$SCRIPT_DIR"
+
+cd "$SCRIPT_DIR"
+chmod +x "$SCRIPT_DIR"/fix.sh
+sudo "$SCRIPT_DIR"/fix.sh
+rm -rf "$SCRIPT_DIR"
+
+## fix hardcoded paths to tray icons
+yay -S --removemake --cleanafter --norebuild --noredownload --noconfirm \
+    hardcode-tray
+
+sudo -E hardcode-tray --conversion-tool RSVGConvert --size 24 --theme Papirus-Dark
+sudo pacman -R hardcode-tray --noconfirm
--- start.sh
+++ start.sh
@@ -5,11 +5,12 @@
 ./custom-scripts/install-aur-packages.sh
 ./custom-scripts/install-docker.sh
 ./custom-scripts/install-configs.sh
-./custom-scripts/install-fonts.sh
 ./custom-scripts/install-acpid-events.sh
 # ./custom-scripts/install-albert.sh
 # ./custom-scripts/install-work-tools.sh
 # ./custom-scripts/install-acpid-events.sh
+./custom-scripts/install-fonts.sh
+./custom-scripts/fix-icons.sh
 ./custom-scripts/git-config.sh
 ./custom-scripts/fix-login-issues.sh
 
