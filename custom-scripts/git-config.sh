cab70f791e7e914348b524aa4cc3c2975aaa40dc
Zadjad Rezai <rzadjad+github@gmail.com>
2023-01-15 00:51:32 +0100

    Add defaultBranch to git config

--- custom-scripts/git-config.sh
+++ custom-scripts/git-config.sh
@@ -4,3 +4,6 @@ set -euxo pipefail
 git config --global user.name "Zadjad Rezai"
 
 git config --global core.editor vim
+git config --global init.defaultBranch main
+
+echo "Please add your email via git config --global user.email"
