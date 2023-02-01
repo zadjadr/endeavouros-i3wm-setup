56cdd841a251cdad243b149c137a82fbdccb1422
Zadjad <zadjad.rezai@ioki.com>
2023-02-01 17:40:56 +0100

    Add sway nvidia compatibility

---
+++ custom-scripts/set-nvidia-sway.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# TODO implement this as a pacman hook to install after every update to sway
+#
+sudo tee /usr/share/wayland-session/sway.desktop > /dev/null <<EOT
+[Desktop Entry]
+Name=Sway
+Comment=An i3-compatible Wayland compositor
+Exec=sway --unsupported-gpu
+Type=Application
+EOT
