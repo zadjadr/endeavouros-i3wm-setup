3b2e829d42bd2b21c45daa22757ab5f543bb6648
--local <zadjad.rezai@ioki.com>
2023-02-04 00:01:33 +0100

    Add further git settings

--- custom-scripts/git-config.sh
+++ custom-scripts/git-config.sh
@@ -5,6 +5,7 @@ git config --global user.name ${1:-'xcalizorz'}
 
 git config --global core.editor vim
 git config --global init.defaultBranch main
+git config --global push.autoSetupRemote true
 
 if [ -z "${2}" ]; then
     echo "Please add your email via git config --global user.email"
