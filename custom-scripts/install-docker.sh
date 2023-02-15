00ab7bd663499266c095cea43d1ae2b3a2b6c84a
xcalizorz <rzadjad+github@gmail.com>
2023-02-15 09:21:28 +0100

    Add IPv6 support to docker

--- custom-scripts/install-docker.sh
+++ custom-scripts/install-docker.sh
@@ -4,3 +4,15 @@ set -exuo pipefail
 sudo pacman -Sy docker docker-buildx docker-compose --needed --noconfirm
 
 sudo usermod -aG docker "$USER"
+
+## Add IPv6 support
+sudo tee /etc/docker/daemon.json > /dev/null <<EOT
+{
+  "ipv6": true,
+  "fixed-cidr-v6": "fd00::/80"
+}
+EOT
+
+sudo ip6tables -t nat -A POSTROUTING -s fd00::/80 ! -o docker0 -j MASQUERADE
+### Test that it works!
+docker run --rm curlimages/curl curl -v -6 example.com
