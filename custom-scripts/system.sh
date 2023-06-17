2abcaea385273a8490bb772038ebb8d8679eb0e7
xcalizorz <rzadjad+github@gmail.com>
2023-06-17 14:48:02 +0200

    Allow special keys to be sent as user in 'video' group

--- custom-scripts/system.sh
+++ custom-scripts/system.sh
@@ -13,3 +13,8 @@ systemctl --user enable dbus-broker
 sudo systemctl restart NetworkManager
 
 echo "A reboot is needed."
+
+## Allow special keys to be sent as user in 'video' group
+sudo tee /etc/udev/rules.d/backlight.rules > /dev/null <<EOT
+ACTION=="add", SUBSYSTEM=="backlight", RUN+="/bin/chgrp video $sys$devpath/brightness", RUN+="/bin/chmod g+w $sys$devpath/brightness"
+EOT
