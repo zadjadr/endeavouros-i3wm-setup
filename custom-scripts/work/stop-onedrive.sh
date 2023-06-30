ecb21829eb1ecfe9d1d3bd20f9b0da1e0a6b3b46
xcalizorz <rzadjad+github@gmail.com>
2023-06-30 05:19:03 +0200

    Improve ondrive stop script

--- custom-scripts/work/stop-onedrive.sh
+++ custom-scripts/work/stop-onedrive.sh
@@ -11,20 +11,61 @@
 #
 set -euxo pipefail
 
-sudo tee ~/.config/systemd/user/stop-onedrive-before-unmount.service > /dev/null <<EOT
+sudo tee /usr/local/bin/stop-onedrive-before-unmount.sh > /dev/null <<"EOT"
+#!/bin/bash
+
+# Function to stop the onedrive service
+stop_onedrive() {
+    systemctl --user stop onedrive
+}
+
+# Function to handle the shutdown signal
+shutdown_handler() {
+    stop_onedrive
+    wait_for_service_stop
+    echo "Shutting down the system..."
+    # Additional cleanup or actions before shutting down
+    shutdown -h now
+}
+
+# Function to handle the reboot signal
+reboot_handler() {
+    stop_onedrive
+    wait_for_service_stop
+    echo "Rebooting the system..."
+    # Additional cleanup or actions before rebooting
+    reboot
+}
+
+# Function to wait for the onedrive service to stop
+wait_for_service_stop() {
+    while [[ $(systemctl --user is-active onedrive) == "active" ]]; do
+        sleep 1
+    done
+}
+
+# Register the shutdown and reboot signal handlers
+trap shutdown_handler SIGINT SIGTERM
+trap reboot_handler SIGUSR1
+EOT
+
+sudo chmod +x /usr/local/bin/stop-onedrive-before-unmount.sh
+
+sudo tee /etc/systemd/system/stop-onedrive-before-unmount.service > /dev/null <<EOT
 [Unit]
-Description=Stop onedrive service before user session manager shutdown
+Description=Stop OneDrive before shutdown or reboot
+DefaultDependencies=no
+Before=shutdown.target reboot.target
 
 [Service]
 Type=oneshot
-ExecStart=/bin/true
-ExecStop=/usr/bin/systemctl --user stop onedrive
+ExecStart=/usr/local/bin/stop-onedrive-before-unmount.sh
 
 [Install]
-WantedBy=default.target
+WantedBy=multi-user.target
 EOT
 
-systemctl --user daemon-reload
-systemctl --user enable stop-onedrive-before-unmount.service
-systemctl --user start stop-onedrive-before-unmount.service
-systemctl --user status stop-onedrive-before-unmount.service
+sudo systemctl daemon-reload
+sudo systemctl enable stop-onedrive-before-unmount.service
+sudo systemctl start stop-onedrive-before-unmount.service
+sudo systemctl status stop-onedrive-before-unmount.service
