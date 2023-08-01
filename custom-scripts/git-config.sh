56e12404b389f0066e97c6c05140b068eff27000
xcalizorz <rzadjad+github@gmail.com>
2023-08-01 12:57:11 +0200

    Improve gitconfig makefile

--- custom-scripts/git-config.sh
+++ custom-scripts/git-config.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-git config --global user.name ${1:-'xcalizorz'}
+git config --global user.name "$1"
 
 git config --global core.editor vim
 git config --global init.defaultBranch main
 git config --global push.autoSetupRemote true
 
 git config --global user.email "$2"
+git config --global core.excludesFile "$HOME/.git/.gitignore"
