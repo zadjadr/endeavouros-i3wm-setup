931e6bb0d89c6ccc4baa071c9188679e03178f12
zadjadr <zadjad.rezai@ioki.com>
2024-03-22 10:55:30 +0100

    Replace mega GUI tool with mega-cmd

--- custom-scripts/install-external-packages.sh
+++ custom-scripts/install-external-packages.sh
@@ -4,6 +4,8 @@ set -euxo pipefail
 DIR=/tmp/
 # Install external packages
 ## Install megasync
-curl -fsSL -o "$DIR"/megasync-x86_64.pkg.tar.zst https://mega.nz/linux/repo/Arch_Extra/x86_64/megasync-x86_64.pkg.tar.zst
-sudo pacman -U "$DIR"/megasync-x86_64.pkg.tar.zst --needed --noconfirm
+curl -fsSL -o "$DIR"/megacmd-x86_64.pkg.tar.zst https://mega.nz/linux/repo/Arch_Extra/x86_64/megacmd-x86_64.pkg.tar.zst
+sudo pacman -U "$DIR"/megacmd-x86_64.pkg.tar.zst --needed --noconfirm
 sudo pacman -Sy
+
+wget  && sudo pacman -U \"$PWD/megacmd-x86_64.pkg.tar.zst\"
