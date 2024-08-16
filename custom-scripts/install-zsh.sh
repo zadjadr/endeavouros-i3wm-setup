81845b33d3a6091f417d0842d64b8b32a811cfbc
zadjadr <zadjad.rezai@ioki.com>
2024-08-16 11:43:52 +0200

    Install starship

--- custom-scripts/install-zsh.sh
+++ custom-scripts/install-zsh.sh
@@ -6,6 +6,7 @@ sudo pacman -Sy --needed --noconfirm zsh zsh-completions
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
 yay -S --removemake --cleanafter --norebuild --noredownload --noconfirm \
+    starship \
     zsh-fast-syntax-highlighting \
     zsh-autosuggestions
 
